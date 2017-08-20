4f6293c7a755aae8ea765c326c4d56fb63c4e3af
John Roche <roche.johnjr@gmail.com>
2017-08-20 08:40:10 -0400

    complete with comments

--- solution/lib/card.rb
+++ solution/lib/card.rb
@@ -1,19 +1,36 @@
 class Card
   RANKS = [:A, 2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K]
+  # ^ RANKS is a constant that comprises our deck
+  # made up of symbols for face cards, numbers for number cards
+
   SUITS = [:clubs, :diamonds, :hearts, :spades]
+  # ^ suits is a constant that sets the four different suits of cards, all symbols
+  # Why do we set these as symbols and not simple strings?
 
   attr_reader :rank, :suit
+  # ^ Here, we declare attr_reader because we do not want people writing to and
+  # changing rank and suit values
 
   def initialize(rank, suit)
     @rank = rank
     @suit = suit
+    # ^ here, we set two instance variables that our Deck Class can employ
   end
 
   def greater_than?(other)
+     # ^ This returns a boolean value
+
+
     RANKS.index(self.rank) > RANKS.index(other.rank)
+    # ^ This compares my hand value compared to dealer's hand value
   end
 
   def ==(other)
     self.rank == other.rank && self.suit == other.suit
+    # If in terms of hand value, it's a push
   end
+
+  # We want to set two methods, greater_than and ==. These intro into the logic of the
+  # blackjack game
+  # Are these the only two methods we need to complete the Blackjack activity?
 end
--- starter_files/lib/deck.rb
+++ starter_files/lib/deck.rb
@@ -1,4 +1,38 @@
 require_relative "card"
+# ^ So this means, essentially, I need to communicate with the Card class
+# and use it's instance variables and methods
+# very similar to us requiring packages in Node
 
 class Deck
+  def initialize
+    @cards = []
+    # Setting of empty of cards to eventually push card values to
+
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
+        # ^ Here, we want to contact the Card class, and iterate through its SUIT
+        # and RANKS constants
+        # Loops through to get us a card (EG 7 of Spades, Ace of Diamonds)
+
+        @cards.push(Card.new(rank, suit))
+        # ^ Once we are dealt a card, we want to push the card to the empty @card array
+      end
+    end
+  end
+
+  def cards_left
+    @cards.length
+    # ^ This tells us the total amount of cards we dealt at that time
+    # ????? Why do we need to know this information?????
+  end
+
+  def draw
+    @cards.shift
+    # Shift the next card into the beginning of our array after drawing
+    # We prefer this to be called "hit"
+  end
+
+  def shuffle
+    @cards.shuffle!
+  end
 end
